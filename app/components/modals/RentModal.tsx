dda721da1fb2cc6ffb79b3d6df39132504da3fb1
Qreamville <Oyeleyebalikis@yahoo.com>
2023-05-15 12:34:29 +0100

    feat: update rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,97 +1,106 @@
 "use client";
 
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import useLoginModal from "../../hooks/useLogin";
-import { signIn } from "next-auth/react";
+import { useMemo, useState } from "react";
 import Modal from "./Modal";
-import Heading from "../Heading";
-import styles from "../../../styles/component/modal.module.scss";
-import Inputs from "../inputs/Inputs";
-import { toast } from "react-hot-toast";
-import Button from "../Button";
-import { useRouter } from "next/navigation";
-import useRegisterModal from "@/app/hooks/useRegister";
 import useRentModal from "@/app/hooks/useRent";
+import Heading from "../Heading";
+import { categories } from "@/app/contants/categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
-  const router = useRouter();
-  const registerModal = useRegisterModal();
   const rentModal = useRentModal();
-  const [isLoading, setIsLoading] = useState(false);
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) return "Create";
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) return undefined;
+    return "Back";
+  }, []);
 
   const {
     register,
     handleSubmit,
+    setValue,
+    watch,
     formState: { errors },
+    reset,
   } = useForm<FieldValues>({
     defaultValues: {
-      email: "",
-      password: "",
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
     },
   });
 
-  // const bodyContent = (
-  //   <div className={styles["register-heading"]}>
-  //     <Heading
-  //       title="Welcome back to Airbnb"
-  //       subtitle="Login to your account!"
-  //       center
-  //     />
-  //     <Inputs
-  //       register={register}
-  //       errors={errors}
-  //       id="email"
-  //       label="Email"
-  //       disabled={isLoading}
-  //       required
-  //     />
-  //     <Inputs
-  //       register={register}
-  //       errors={errors}
-  //       id="password"
-  //       label="Password"
-  //       disabled={isLoading}
-  //       required
-  //     />
-  //   </div>
-  // );
+  const category = watch("category");
 
-  // const footerContent = (
-  //   <div className={styles["register-footer"]}>
-  //     <Button
-  //       outline
-  //       label="Continue with Google"
-  //       icon={FcGoogle}
-  //       onClick={() => signIn("google")}
-  //     />
-  //     <Button
-  //       outline
-  //       label="Continue with Github"
-  //       icon={AiFillGithub}
-  //       onClick={() => signIn("github")}
-  //     />
-  //     <div>
-  //       <div className={styles["have-account"]}>
-  //         <div>First time using Airbnb?</div>
-  //         <div onClick={toggle}>Create an account</div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describe your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div className="col-span-1" key={item.label}>
+            <CategoryInput
+              onclick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   return (
     <Modal
-      disabled={isLoading}
       isOpen={rentModal.isOpen}
       title="Airbnb your home"
-      actionLabel="Submit"
+      actionLabel={actionLabel}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      // body={bodyContent}
-      // footer={footerContent}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      body={bodyContent}
     />
   );
 };
